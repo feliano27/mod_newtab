88263c6778e332b49286e2786ebe3a34d0964407
feliano27 <feliano27@gmail.com>
2014-07-13 11:34:02 +0200

    hopefully this commit fixes the problem...

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ var bootstrap = function(port) {
     breach.expose('init', function(src, args, cb_) {
       breach.module('core').call('tabs_new_tab_url', { 
         url: 'http://127.0.0.1:' + port + '/newtab'
-        alert("probably a new tab!!");
       }, function(err) {
         console.log('New tab page set! [' + err + ']');
       });
